4278a7e07de7ab564e43c6dee3b96ce7c47afe72
Freddy <freddy.delgado.porras@gmail.com>
2021-03-17 13:37:49 +0100

    ++

---
+++ Basic3/script.js
@@ -0,0 +1,4 @@
+var fullname = ["Freddy", "Kasimir", "Lesia", "Carlos"];
+fullname.forEach(function (value, index) {
+    console.log("your name is: " + value + " \n the index i: " + index + " \n and both are: " + value + ", " + index);
+}); // save
