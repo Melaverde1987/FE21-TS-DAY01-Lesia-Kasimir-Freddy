6f2bb64dad8b3a9cac9a42ffbd632fa015533e7d
Freddy <freddy.delgado.porras@gmail.com>
2021-03-17 13:37:27 +0100

    news

---
+++ Basic3/script.ts
@@ -0,0 +1,7 @@
+var fullname : Array <string> = ["Freddy", "Kasimir", "Lesia", "Carlos"];
+
+fullname.forEach(function(value,index){
+
+console.log(`your name is: ${value} \n the index i: ${index} \n and both are: ${value}, ${index}`);
+
+});// save
