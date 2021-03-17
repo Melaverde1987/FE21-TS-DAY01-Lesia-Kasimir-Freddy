d9d7718f3c35a7d1d04bdf77e1df0e4229656e71
Attila <kasimir_vda@icloud.com>
2021-03-17 13:03:02 +0100

    ad comments to exercise 1

--- Basic/script.js
+++ Basic/script.js
@@ -1,9 +1,21 @@
-var ourArrayA = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (var i = 0; i < 10; i++) {
+// create an array that alows only numbers
+var ourArrayA = [];
+// fill the array with numbers from 1 to 10 with a loop that uses the push() function
+for (var k = 1; k < 11; k++) {
+    ourArrayA.push(k);
+}
+// multiplying 1 to 10 with 1 to 10
+for (var i = 0; i < ourArrayA.length; i++) {
+    // create a variable that takes every loop the value of the index
     var a = ourArrayA[i];
-    for (var j = 0; j < 10; j++) {
+    // a is chosen and before it loops to another value we step in the array again
+    // a should be multiplied with every value out of the array
+    for (var j = 0; j < ourArrayA.length; j++) {
         var b = ourArrayA[j];
         var result = a * b;
+        // print the result to the console in a table 
         console.table(a + " X " + b + " = " + result);
     }
+    // make a line break after every table
+    console.table('\n');
 }
--- Basic/script.ts
+++ Basic/script.ts
@@ -1,12 +1,27 @@
 
+// create an array that alows only numbers
+var ourArrayA : Array<number> = [];
 
-var ourArrayA : Array<number> = [1,2,3,4,5,6,7,8,9,10];
+// fill the array with numbers from 1 to 10 with a loop that uses the push() function
+for (let k = 1; k < 11; k++) {
+    ourArrayA.push(k)
+}
 
-for (let i = 0; i < 10; i++) {
+// multiplying 1 to 10 with 1 to 10
+for (let i = 0; i < ourArrayA.length; i++) {
+
+    // create a variable that takes every loop the value of the index
     var a = ourArrayA[i];
-    for (let j = 0; j < 10; j++) {
+
+    // a is chosen and before it loops to another value we step in the array again
+    // a should be multiplied with every value out of the array
+    for (let j = 0; j < ourArrayA.length; j++) {
         var b = ourArrayA[j];
         var result = a*b;
+        // print the result to the console in a table 
         console.table(`${a} X ${b} = ${result}`);
     }
+
+    // make a line break after every table
+    console.table('\n');
 }
